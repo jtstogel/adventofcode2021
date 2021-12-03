7125bc223d5093e142f1329d8311c883c21c8e16
Jackson Stogel <jtstogel@berkeley.edu>
2021-12-02 23:18:55 -0800

    day04 boilerplate

--- src/adventofcode2021/core.clj
+++ src/adventofcode2021/core.clj
@@ -4,7 +4,9 @@
             [adventofcode2021.day02.part1]
             [adventofcode2021.day02.part2]
             [adventofcode2021.day03.part1]
-            [adventofcode2021.day03.part2])
+            [adventofcode2021.day03.part2]
+            [adventofcode2021.day04.part1]
+            [adventofcode2021.day04.part2])
   (:gen-class))
 
 (def solutions
@@ -16,7 +18,10 @@
     "part2" adventofcode2021.day02.part2/solution}
    "day03"
    {"part1" adventofcode2021.day03.part1/solution
-    "part2" adventofcode2021.day03.part2/solution}})
+    "part2" adventofcode2021.day03.part2/solution}
+   "day04"
+   {"part1" adventofcode2021.day04.part1/solution
+    "part2" adventofcode2021.day04.part2/solution}})
 
 (defn run-subdir
   [[day part input-file]]
---
+++ src/adventofcode2021/day04/part1.clj
@@ -0,0 +1,13 @@
+(ns adventofcode2021.day04.part1)
+
+(defn parse
+  [text]
+  (->> text))
+
+(defn solve
+  [value]
+  value)
+
+(def solution
+  {:parse parse
+   :solve solve})
