aeb35a54aa643108b66742b0d0c3cd64fb2c2588
Jackson Stogel <jtstogel@berkeley.edu>
2021-12-03 23:45:26 -0800

    import day04.part1 instead of copying the implementation

--- src/adventofcode2021/day04/part2.clj
+++ src/adventofcode2021/day04/part2.clj
@@ -1,20 +1,6 @@
 (ns adventofcode2021.day04.part2
-  (:require [clojure.string]))
-
-(defn parse-int [x] (Integer/parseInt x))
-(defn parse-ints [sep x] (map parse-int (filter not-empty (clojure.string/split x sep))))
-(defn parse-board [board-text]
-  (->> board-text
-       (clojure.string/split-lines)
-       (map (partial parse-ints #"\s+"))))
-(defn parse-calls [calls-text] (parse-ints #"," calls-text))
-
-(defn parse
-  [text]
-  (let [[calls-text & board-texts] (clojure.string/split text #"\n\n")
-        calls (parse-calls calls-text)
-        boards (map parse-board board-texts)]
-    [calls boards]))
+  (:require [clojure.string]
+            [adventofcode2021.day04.part1 :as part1]))
 
 (defn tails
   [seq]
@@ -29,36 +15,15 @@
        (reverse)
        (rest)))
 
-(defn transpose
-  [seqs]
-  (apply map vector seqs))
-
-(defn candidate-number-combos
-  "Returns a sequence of numbers from the bingo board that could constitute a win."
-  [board]
-  (let [rows board
-        cols (transpose board)]
-    (concat rows cols)))
-
-(defn bingo-score
-  [calls board]
-  (let [called? (set calls)
-        candidate-wins? (fn [candidate] (every? called? candidate))
-        candidates (candidate-number-combos board)
-        winning-board? (some candidate-wins? candidates)
-        unmarked-numbers (filter (comp not called?) (flatten board))]
-    (when winning-board?
-      (* (apply + unmarked-numbers) (last calls)))))
-
 (defn winning-bingo-boards
   [calls boards]
-  (let [winning? (fn [board] (not (nil? (bingo-score calls board))))]
+  (let [winning? (fn [board] (not (nil? (part1/bingo-score calls board))))]
     (filter winning? boards)))
 
 (defn first-bingo-score
   [all-calls board]
   (->> (partials all-calls)
-       (map #(bingo-score % board))
+       (map #(part1/bingo-score % board))
        (filter (comp not nil?))
        (first)))
 
@@ -72,5 +37,5 @@
        (first-bingo-score calls)))
 
 (def solution
-  {:parse parse
+  {:parse part1/parse
    :solve solve})
