b7e7606fe636de7784dbdedc42334810db041d7f
Jackson Stogel <jtstogel@berkeley.edu>
2021-12-06 21:25:10 -0800

    day07

--- src/adventofcode2021/day07/part1.clj
+++ src/adventofcode2021/day07/part1.clj
@@ -1,14 +1,23 @@
 (ns adventofcode2021.day07.part1
   (:require [clojure.string]
-            [clojure.set]))
+            [clojure.set]
+            [clojure.math.numeric-tower :as math]))
 
+(defn parse-int [text] (Integer/parseInt text))
 (defn parse
   [text]
-  text)
+  (map parse-int (clojure.string/split text #",")))
+
+(defn fuel-cost
+  [horizontal-positions unified-position]
+  (->> horizontal-positions
+       (map (comp math/abs (partial - unified-position)))
+       (apply +)))
 
 (defn solve
-  [value]
-  value)
+  [horizontal-positions]
+  (let [best-position (apply min-key (partial fuel-cost horizontal-positions) (range 1 2000))]
+    (fuel-cost horizontal-positions best-position)))
 
 (def solution
   {:parse parse
--- src/adventofcode2021/day07/part2.clj
+++ src/adventofcode2021/day07/part2.clj
@@ -1,11 +1,27 @@
 (ns adventofcode2021.day07.part2
   (:require [clojure.string]
             [clojure.set]
+            [clojure.math.numeric-tower :as math]
             [adventofcode2021.day07.part1 :as part1]))
 
+(defn triangle-number
+  [n]
+  (quot (* (inc n) n) 2))
+
+(defn distance
+  [from to]
+  (triangle-number (math/abs (- from to))))
+
+(defn fuel-cost
+  [horizontal-positions unified-position]
+  (->> horizontal-positions
+       (map (comp (partial distance unified-position)))
+       (apply +)))
+
 (defn solve
-  [value]
-  value)
+  [horizontal-positions]
+  (let [best-position (apply min-key (partial fuel-cost horizontal-positions) (range 1 1500))]
+    (fuel-cost horizontal-positions best-position)))
 
 (def solution
   {:parse part1/parse
