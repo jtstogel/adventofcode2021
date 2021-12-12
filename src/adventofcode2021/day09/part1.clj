ce901853b614f461b1783814bffc0d610486b558
Jackson Stogel <jtstogel@berkeley.edu>
2021-12-12 14:12:52 -0800

    day09

--- src/adventofcode2021/day09/part1.clj
+++ src/adventofcode2021/day09/part1.clj
@@ -3,23 +3,40 @@
             [clojure.set]))
 
 (defn parse-int [text] (Integer/parseInt text))
-(defn parse ; parses comma-separated list of integers
-  [text]
-  (map parse-int (clojure.string/split text #",")))
-
 (defn parse-line
   [text]
-  text)
+  (vec (map parse-int (clojure.string/split text #""))))
 (defn parse
-  [text] ; parses lines of things
+  [text]
   (->>  text
         (clojure.string/split-lines)
-        (map parse-line)))
+        (map parse-line)
+        (vec)))
 
+(defn mat->map
+  [mat]
+  (apply merge (for [i (range (count mat))
+                     j (range (count (mat i)))]
+                 {[i j] (get-in mat [i j])})))
+
+(defn neighbor-positions
+  [[i j]]
+  [[i (inc j)] [(inc i) j] [i (dec j)] [(dec i) j]])
+
+(defn find-basin-positions
+  [pos->height]
+  (letfn [(basin?
+           [pos]
+           (let [height (pos->height pos)
+                 neighbor-heights (map #(get pos->height % ##Inf) (neighbor-positions pos))]
+             (< height (apply min neighbor-heights))))]
+    (filter basin? (keys pos->height))))
 
 (defn solve
-  [value]
-  value)
+  [mat]
+  (let [pos->height (mat->map mat)]
+    (apply + (map (comp inc pos->height) 
+                  (find-basin-positions pos->height)))))
 
 (def solution
   {:parse parse
--- src/adventofcode2021/day09/part2.clj
+++ src/adventofcode2021/day09/part2.clj
@@ -3,9 +3,32 @@
             [clojure.set]
             [adventofcode2021.day09.part1 :as part1]))
 
+(defn lower-point
+  [pos->height pos]
+  (apply min-key #(get pos->height % ##Inf)
+         pos
+         (part1/neighbor-positions pos)))
+
+(def find-basin
+  (memoize
+   (fn [pos->height pos]
+     (let [next-pos (lower-point pos->height pos)]
+       (if (= pos next-pos)
+         pos
+         (find-basin pos->height next-pos))))))
+
+(defn all-basins
+  [pos->height]
+  (let [all-positions (keys pos->height)
+        non-nine-positions (filter #(not= 9 (get pos->height %)) all-positions)
+        basin+position (map #(hash-map (find-basin pos->height %) #{(vec %)}) non-nine-positions)
+        low-point->positions (apply merge-with clojure.set/union basin+position)
+        basins (vals low-point->positions)]
+    (apply * (take 3 (sort > (map count basins))))))
+
 (defn solve
-  [value]
-  value)
+  [mat]
+  (all-basins (part1/mat->map mat)))
 
 (def solution
   {:parse part1/parse
