6685a70091f2e595974ee8aa9bcd6a7cadb8b6a1
Jackson Stogel <jtstogel@berkeley.edu>
2021-12-12 13:46:41 -0800

    day12

--- src/adventofcode2021/day12/part1.clj
+++ src/adventofcode2021/day12/part1.clj
@@ -2,13 +2,38 @@
   (:require [clojure.string]
             [clojure.set]))
 
+(defn parse-line
+  [text]
+  (clojure.string/split text #"-"))
+(defn rm-start [caves] (clojure.set/difference caves #{"start"}))
 (defn parse
   [text]
-  text)
+  (->> text
+       (clojure.string/split-lines)
+       (map parse-line)
+       (map (fn [[a b]] {a (rm-start #{b}), b (rm-start #{a})}))
+       (apply merge-with clojure.set/union)))
+
+(defn small-cave? [name]
+  (= name (clojure.string/lower-case name)))
+
+(defn count-paths
+  ([cave->adjacent]
+   (count-paths cave->adjacent #{} "start"))
+  ([cave->adjacent visited current-cave]
+   (if (= "end" current-cave)
+     1
+     (let [adjacent-caves (cave->adjacent current-cave)
+           visited-small-caves (clojure.set/select small-cave? visited)
+           next-cave-candidates (clojure.set/difference adjacent-caves visited-small-caves)
+           visited-inc-curr (clojure.set/union #{current-cave} visited)]
+       (->> next-cave-candidates
+            (map (partial count-paths cave->adjacent visited-inc-curr))
+            (apply +))))))
 
 (defn solve
-  [value]
-  value)
+  [cave->adjacent]
+  (count-paths cave->adjacent))
 
 (def solution
   {:parse parse
--- src/adventofcode2021/day12/part2.clj
+++ src/adventofcode2021/day12/part2.clj
@@ -3,9 +3,31 @@
             [clojure.set]
             [adventofcode2021.day12.part1 :as part1]))
 
+(defn visitable? [visit-count name]
+  (let [big? (not (part1/small-cave? name))
+        small-caves (filter part1/small-cave? (keys visit-count))
+        small-cave-visisted-twice? (some #(<= 2 %) (map visit-count small-caves))
+        times-visited (get visit-count name 0)]
+    (or big?
+        (= times-visited 0)
+        (and (= times-visited 1) (not small-cave-visisted-twice?)))))
+
+(defn paths
+  ([cave->adjacent]
+   (paths cave->adjacent {} "start"))
+  ([cave->adjacent visit-count current-cave]
+   (if (= "end" current-cave)
+     1
+     (let [adjacent-caves (cave->adjacent current-cave)
+           visit-count-incl-curr (update visit-count current-cave (fnil inc 0))
+           next-cave-candidates (clojure.set/select (partial visitable? visit-count-incl-curr) adjacent-caves)]
+       (->> next-cave-candidates
+            (map (partial paths cave->adjacent visit-count-incl-curr))
+            (apply +))))))
+
 (defn solve
-  [value]
-  value)
+  [cave->adjacent]
+  (paths cave->adjacent))
 
 (def solution
   {:parse part1/parse
