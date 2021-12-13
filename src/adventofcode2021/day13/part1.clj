dfab8967ca868f1f9c93c6d75996b91ac2c41b6a
Jackson Stogel <jtstogel@berkeley.edu>
2021-12-12 21:52:25 -0800

    day 13

--- src/adventofcode2021/day13/part1.clj
+++ src/adventofcode2021/day13/part1.clj
@@ -2,13 +2,42 @@
   (:require [clojure.string]
             [clojure.set]))
 
+(defn parse-coord
+  [text]
+  (vec (map #(Integer/parseInt %) (clojure.string/split text #","))))
+
+(defn parse-instruction
+  [text]
+  (let [spec (subs text (count "fold along "))
+        [axis-text n] (clojure.string/split spec #"=")]
+    {:axis (keyword axis-text)
+     :n (Integer/parseInt n)}))
+
 (defn parse
   [text]
-  text)
+  (let [[coords-text instructions] (clojure.string/split text #"\n\n")]
+    {:coordinates (set (map parse-coord (clojure.string/split-lines coords-text)))
+     :instructions (vec (map parse-instruction (clojure.string/split-lines instructions)))}))
+
+(defn fold-coordinate
+  [{:keys [axis n]} [x y]]
+  (case axis
+    :x (when (not= n x)
+         [(if (< x n) x (- (* 2 n) x)) y])
+    :y (when (not= n y)
+         [x (if (< y n) y (- (* 2 n) y))])))
+
+(defn fold
+  [coordinates instruction]
+  (set (map (partial fold-coordinate instruction) coordinates)))
+
+(defn do-folds
+  [coordinates instructions]
+  (reduce fold coordinates instructions))
 
 (defn solve
-  [value]
-  value)
+  [{:keys [coordinates instructions]}]
+  (count (do-folds coordinates (take 1 instructions))))
 
 (def solution
   {:parse parse
--- src/adventofcode2021/day13/part2.clj
+++ src/adventofcode2021/day13/part2.clj
@@ -3,9 +3,20 @@
             [clojure.set]
             [adventofcode2021.day13.part1 :as part1]))
 
+(defn fmt-coordinates
+  [coordinates]
+  (let [width (inc (apply max (map first coordinates)))
+        height (inc (apply max (map second coordinates)))]
+    (clojure.string/join
+     "\n"
+     (for [y (range height)]
+       (clojure.string/join
+        (for [x (range width)]
+          (if (coordinates [x y]) "#" ".")))))))
+
 (defn solve
-  [value]
-  value)
+  [{:keys [coordinates instructions]}]
+  (fmt-coordinates (part1/do-folds coordinates instructions)))
 
 (def solution
   {:parse part1/parse
