61c09ce357f39f3f212c9c848ac2e021ccff340d
jtstogel <jtstogel@berkeley.edu>
2021-12-15 07:07:39 +0000

    day15

--- src/adventofcode2021/day15/part1.clj
+++ src/adventofcode2021/day15/part1.clj
@@ -1,14 +1,55 @@
 (ns adventofcode2021.day15.part1
-  (:require [clojure.string]
-            [clojure.set]))
+  (:require [clojure.string :as str]
+            [clojure.math.combinatorics :as combo]
+            [clojure.set]
+            [astar.core :as astar]))
 
+(defn parse-line
+  [text]
+  (->> (str/split text #"")
+       (mapv #(Integer/parseInt %) )))
 (defn parse
   [text]
-  text)
+  (->> text
+       (str/split-lines)
+       (mapv parse-line)))
+
+(defn mat-shape [mat] [(count mat) (count (mat 0))])
+
+(defn in-bounds?
+  [[height width] [i j]]
+  (and (< i height)
+       (<= 0 i)
+       (< j width)
+       (<= 0 j)))
+
+(defn neighbor-positions
+  [shape [i j]]
+  (filterv (partial in-bounds? shape)
+           [[i (inc j)] [(inc i) j] [i (dec j)] [(dec i) j]]))
+
+(defn graphify
+  [mat]
+  (let [[height width :as shape] (mat-shape mat)
+        all-positions (map vec (combo/cartesian-product (range height) (range width)))]
+    {:graph (apply hash-map (mapcat #(vector % (neighbor-positions shape %)) all-positions))
+     :dist (fn [_ to] (get-in mat to))
+     :h (fn [_] 0)}))
+
+(defn lowest-risk-path
+  [mat]
+  (let [[height width] (mat-shape mat)
+        {:keys [graph dist h]} (graphify mat)]
+    (astar/route graph dist h [0 0] [(dec height) (dec width)])))
+
+(defn path-total-risk
+  [mat path]
+  (let [risk #(get-in mat %)]
+    (apply + (map risk path))))
 
 (defn solve
-  [value]
-  value)
+  [mat]
+  (path-total-risk mat (lowest-risk-path mat)))
 
 (def solution
   {:parse parse
--- src/adventofcode2021/day15/part2.clj
+++ src/adventofcode2021/day15/part2.clj
@@ -1,11 +1,28 @@
 (ns adventofcode2021.day15.part2
-  (:require [clojure.string]
-            [clojure.set]
-            [adventofcode2021.day15.part1 :as part1]))
+  (:require [adventofcode2021.day15.part1 :as part1]))
+
+(defn incv
+  "Increments every element of coll by n, rolling over to 1 when exceeding 9."
+  [coll]
+  (mapv #(inc (mod % 9)) coll))
+
+(defn expand
+  [times f v]
+  (->> (iterate f v)
+       (take times)
+       (apply concat)
+       (vec)))
+
+(defn expand-mat
+  [n mat]
+  (->> mat
+       (map (partial expand n incv))    ; expand horizontally n times
+       (expand n (partial mapv incv)))) ; expand vertically n times
 
 (defn solve
-  [value]
-  value)
+  [mat]
+  (let [expanded (expand-mat 5 mat)]
+    (part1/path-total-risk expanded (part1/lowest-risk-path expanded))))
 
 (def solution
   {:parse part1/parse
