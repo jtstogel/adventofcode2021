7205660487ebcbd93d668522839ba88698286ff3
jtstogel <jtstogel@berkeley.edu>
2021-12-17 15:10:10 -0800

    day 10

--- src/adventofcode2021/day10/part1.clj
+++ src/adventofcode2021/day10/part1.clj
@@ -4,11 +4,42 @@
 
 (defn parse
   [text]
-  text)
+  (clojure.string/split-lines text))
+
+(def open->closed {\{ \}, \[ \], \< \>, \( \)})
+
+(defn process-chunk
+  [{:keys [closing-chunks remaining-chunks] :as state}]
+  (when-let [chunk (peek remaining-chunks)]
+    (cond
+      (open->closed chunk)
+      (assoc state
+             :closing-chunks (conj closing-chunks (open->closed chunk))
+             :remaining-chunks (pop remaining-chunks))
+      (= (peek closing-chunks) chunk)
+      (assoc state
+             :closing-chunks (pop closing-chunks)
+             :remaining-chunks (pop remaining-chunks))
+      :else nil)))
+
+(defn process-chunks
+  [chunks]
+  (let [initial {:closing-chunks '(), :remaining-chunks (apply list chunks)}]
+    (last (take-while (comp not nil?) (iterate process-chunk initial)))))
+
+(def chunk->score {\) 3, \] 57, \} 1197, \> 25137})
+
+(defn score-illegal
+  [chunks]
+  (let [{:keys [remaining-chunks]} (process-chunks chunks)]
+    (when (seq remaining-chunks)
+      (chunk->score (peek remaining-chunks)))))
 
 (defn solve
-  [value]
-  value)
+  [chunks-coll]
+  (->> chunks-coll
+       (keep score-illegal)
+       (apply +)))
 
 (def solution
   {:parse parse
--- src/adventofcode2021/day10/part2.clj
+++ src/adventofcode2021/day10/part2.clj
@@ -3,9 +3,30 @@
             [clojure.set]
             [adventofcode2021.day10.part1 :as part1]))
 
+(def chunk->score {\) 1, \] 2, \} 3, \> 4})
+
+(defn score-closing-chunks
+  [closing-chunks]
+  (reduce (fn [acc chunk] (+ (chunk->score chunk) (* 5 acc)))
+          0
+          closing-chunks))
+
+(defn score-unfinished
+  [chunks]
+  (let [{:keys [remaining-chunks closing-chunks]} (part1/process-chunks chunks)]
+    (when (empty? remaining-chunks)
+      (score-closing-chunks closing-chunks))))
+
+(defn median
+  "Returns the median of a collection of comparable elements, or nil if coll is empty."
+  [coll]
+  (nth (sort coll) (quot (count coll) 2) nil))
+
 (defn solve
-  [value]
-  value)
+  [chunks-coll]
+  (->> chunks-coll
+       (keep score-unfinished)
+       (median)))
 
 (def solution
   {:parse part1/parse
