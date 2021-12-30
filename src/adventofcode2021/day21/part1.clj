613b7cd96212287bf9cb47e03bfcbd766eb011cf
Jackson Stogel <jtstogel@berkeley.edu>
2021-12-29 18:04:25 -0800

    day21

--- src/adventofcode2021/day21/part1.clj
+++ src/adventofcode2021/day21/part1.clj
@@ -4,11 +4,44 @@
 
 (defn parse
   [text]
-  text)
+  (let [[_ p1 _ p2] (re-seq #"\d+" text)]
+    {:p1 {:pos (Integer/parseInt p1)
+          :score 0}
+     :p2 {:pos (Integer/parseInt p2)
+          :score 0}
+     :curr-player :p1}))
+
+(defn move-player
+  [pos distance]
+  (inc (rem (dec (+ distance pos)) 10)))
+
+(def other-player {:p1 :p2, :p2 :p1})
+
+(defn make-move
+  [{:keys [curr-player] :as game} distance]
+  (let [next-pos (move-player (get-in game [curr-player :pos]) distance)
+        next-score (+ (get-in game [curr-player :score]) next-pos)]
+    (assoc game
+           :curr-player (other-player curr-player)
+           curr-player (assoc (game curr-player)
+                              :pos next-pos
+                              :score next-score))))
+
+(defn play-game
+  [game-init]
+  (let [rolled-distances (->> (apply concat (repeat (range 1 101)))
+                              (partition 3)
+                              (map (partial apply +)))]
+    (reductions make-move game-init rolled-distances)))
 
 (defn solve
-  [value]
-  value)
+  [game-init]
+  (let [game-states (take-while #(and (< (get-in % [:p1 :score]) 1000) (< (get-in % [:p2 :score]) 1000))
+                                (play-game game-init))
+        roll-count (* 3 (count game-states))
+        final (last game-states)
+        loser-score (min (get-in final [:p1 :score]) (get-in final [:p2 :score]))]
+    (* roll-count loser-score)))
 
 (def solution
   {:parse parse
--- src/adventofcode2021/day21/part2.clj
+++ src/adventofcode2021/day21/part2.clj
@@ -3,9 +3,38 @@
             [clojure.set]
             [adventofcode2021.day21.part1 :as part1]))
 
+(def distance->universe-count
+  (frequencies
+   (for [a (range 1 4), b (range 1 4), c (range 1 4)]
+     (+ a b c))))
+
+(defn make-nondeterministic-move
+  [game-state->universe-count]
+  (apply merge-with +
+         (for [[game game-uni-cnt] game-state->universe-count
+               [dist dist-uni-cnt] distance->universe-count]
+           {(part1/make-move game dist) (* game-uni-cnt dist-uni-cnt)})))
+
+(defn winner
+  "Returns the winner or nil if the game is not yet over."
+  [game]
+  (first (filter #(<= 21 (get-in game [% :score])) [:p1 :p2])))
+
 (defn solve
-  [value]
-  value)
+  [game-init]
+  (loop [unfinised-game->universe-count {game-init 1}
+         p1-total 0
+         p2-total 0]
+    (if (empty? unfinised-game->universe-count)
+      (max p1-total p2-total)
+      (let [game->universe-count (make-nondeterministic-move unfinised-game->universe-count)
+            {p1-won :p1
+             p2-won :p2
+             unfinished nil} (group-by winner (keys game->universe-count))]
+        (recur
+         (select-keys game->universe-count unfinished)
+         (apply + p1-total (vals (select-keys game->universe-count p1-won)))
+         (apply + p2-total (vals (select-keys game->universe-count p2-won))))))))
 
 (def solution
   {:parse part1/parse
